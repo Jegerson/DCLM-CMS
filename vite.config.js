2a419c46334b756b299b6a5872f658da3a45bf4d
Jegerson <71879616+Jegerson@users.noreply.github.com>
2025-08-29 16:43:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,9 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:5001',
     },
+    // Add the specific host to the allowed hosts list
+    allowedHosts: [
+      'fc666cb4-8a7c-4a2e-ada0-d5cfb72f5fa8-00-3f9zhd5okic82.janeway.replit.dev',
+    ],
   },
 })
